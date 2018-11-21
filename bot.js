f9a3e0e4990debc2fdf72ee62b4ca0dd082712f6
autoclickering <44882252+autoclickering@users.noreply.github.com>
2018-11-21 15:22:39 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -196,4 +196,33 @@ const channel = "514939221715189781";//ايدي الروم
 
 
 
+
+
+
+client.on('ready', function(){    
+    var ms = 4000 ;    
+    var setGame = [`Premuim.`,"»Top","»Welcome Back","TEAM","»D"];    
+    var i = -1;    
+    var j = 0;    
+    setInterval(function (){    
+        if( i == -1 ){    
+j = 1;    
+       }    
+        if( i == (setGame.length)-1 ){    
+            j = -1;    
+      }    
+       i = i+j;    
+        client.user.setGame(setGame[i],`http://twitch.tv/quastyle11`);    
+}, ms);    
+    
+});
+
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN)
