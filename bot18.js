9d1b7c23c66acd7e16067a9f893a8fc32c2b49bb
autoclickering <44882252+autoclickering@users.noreply.github.com>
2018-11-21 14:51:57 -0800

    Update bot18.js

--- bot18.js
+++ bot18.js
@@ -160,4 +160,29 @@ function timerFunc() {
 }
 
 
+
+client.on('ready', function(){    
+    var ms = 4000 ;    
+    var setGame = [`»Premium..`,"»Top","Welcome Back","♥"];    
+    var i = -1;    
+    var j = 0;    
+    setInterval(function (){    
+        if( i == -1 ){    
+j = 1;    
+       }    
+        if( i == (setGame.length)-1 ){    
+            j = -1;    
+      }    
+       i = i+j;    
+        client.user.setGame(setGame[i],`http://twitch.tv/quastyle11`);    
+}, ms);    
+    
+});
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN)
